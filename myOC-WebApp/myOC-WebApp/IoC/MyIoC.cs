d4c896c820f7451d3b7f77a870f0a24b5cedf35f
Daniel Wood <daniel.a.wood.jr@gmail.com>
2015-07-29 23:54:31 -0600

    trying out some stuff

--- myOC-WebApp/myOC-WebApp/IoC/MyIoC.cs
+++ myOC-WebApp/myOC-WebApp/IoC/MyIoC.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 
 namespace myOC_WebApp.IoC
@@ -14,13 +15,43 @@ namespace myOC_WebApp.IoC
         {
             types[typeof(IType)] = typeof(OType);
         }
+
         public static void Register<IType, OType>(OType instance)
         {
             objects[typeof(IType)] = instance;
         }
+
+        public static T Resolve<T>()
+        {
+            return (T)Resolve(typeof(T));
+        }
+
         public static object Resolve(Type contract)
         {
-            return "cheese!!<3!!";
+            
+            if (objects.ContainsKey(contract))
+            {
+                return objects[contract];
+            }
+            else
+            {
+                Type implementation = types[contract];
+                ConstructorInfo constructor = implementation.GetConstructors()[0];
+                ParameterInfo[] constructorParameters = constructor.GetParameters();
+
+                if (constructorParameters.Length == 0)
+                {
+                    return Activator.CreateInstance(implementation);
+                }
+
+                List<object> parameters = new List<object>(constructorParameters.Length);
+
+                foreach (ParameterInfo parameterInfo in constructorParameters)
+                {
+                    parameters.Add(Resolve(parameterInfo.ParameterType));
+                }
+                return constructor.Invoke(parameters.ToArray());
+            }
         }
     }
 }
